51f5afc6e7e16d916fdedae2749b0a960b1db8f9
Gabriel543 <51121742019.01@senacrjedu.local>
2019-08-23 21:35:34 -0300

    Update registrar.php

--- aula04/registrar.php
+++ aula04/registrar.php
@@ -5,10 +5,15 @@ $preco = $_POST['txtQtd'];
 $quantidade = $_POST['txtPreco'];
 $imagem = $_POST['flImg'];*/
 
-$nome = filter_input(INPUT_POST, "txtNome");
-$preco = filter_input(INPUT_POST, "txtQtd");
-$quantidade = filter_input(INPUT_POST, "txtPreco");
-$imagem = filter_input(INPUT_POST, "flImg");
+
+If($_SERVER["REQUEST_METHOD"] === "POST"){
+    $nome = filter_input(INPUT_POST, "txtNome") ?? "";
+    $preco = filter_input(INPUT_POST, "txtQtd");
+    $quantidade = filter_input(INPUT_POST, "txtPreco");
+    $imagem = filter_input(INPUT_POST, "flImg");
 
 
-echo "$nome <br> $quantidade <br> $preco <br> $imagem";
+    echo "$nome <br> $quantidade <br> $preco <br> $imagem";
+}else{
+    header("location: cadastrar.php");
+}
